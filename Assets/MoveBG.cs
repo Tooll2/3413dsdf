57ba915a2f2235764b06f784007e6c1502d9cc3e
Семен Волков <nemocema2@gmail.com>
2020-04-11 06:07:43 +0500

    Начал делать главное меню...

---
+++ Assets/MoveBG.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBG : MonoBehaviour
+{   
+    public Transform currentTargetVector, start, myTransform;
+    public float movementSpeed;
+    private bool isLeft = true;
+
+
+    void FixedUpdate()
+    {
+        if (isLeft)
+        {
+            myTransform.position = Vector2.MoveTowards(myTransform.position, currentTargetVector.position, Time.fixedDeltaTime * movementSpeed);
+            if (myTransform.position == currentTargetVector.position)
+                isLeft = false;
+        }
+        else if (!isLeft) 
+        {
+            myTransform.position = Vector2.MoveTowards(myTransform.position, start.position, Time.fixedDeltaTime * movementSpeed);
+            if (myTransform.position == start.position)
+                isLeft = true;
+        }
+    }
+}
