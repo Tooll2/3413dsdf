74201efefcd5759a0647c59b19662d230b215525
Семен Волков <nemocema2@gmail.com>
2020-04-12 06:25:57 +0500

    RyКнопки в меню

---
+++ Assets/Scripts/ButtonsMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonsMainMenu : MonoBehaviour
+{
+    public void SingleButton()
+    {
+        SceneManager.LoadScene("Single");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
